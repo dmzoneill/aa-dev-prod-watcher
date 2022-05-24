878ebdc046a671e6c02a0d711db8e42394467c89
David O Neill <daoneill@redhat.com>
2022-05-24 09:02:40 +0100

    Error firlendly

--- backend/scm.go
+++ backend/scm.go
@@ -8,6 +8,7 @@ import (
 	"os/exec"
 	"strconv"
 	"strings"
+	"time"
 )
 
 func execute(app string, dir string, args []string) string {
@@ -17,52 +18,80 @@ func execute(app string, dir string, args []string) string {
 		os.Exit(2)
 	}
 
-	cmd := exec.Command(app, args...)
+	for i := 0; i < 2; i++ {
+		cmd := exec.Command(app, args...)
 
-	var out bytes.Buffer
-	var stderr bytes.Buffer
-	cmd.Stdout = &out
-	cmd.Stderr = &stderr
+		var out bytes.Buffer
+		var stderr bytes.Buffer
+		cmd.Stdout = &out
+		cmd.Stderr = &stderr
 
-	if args[0] != "clone" {
-		cmd.Dir = path + "/repos/" + dir
-	} else {
-		cmd.Dir = path
+		if args[0] != "clone" {
+			cmd.Dir = path + "/repos/" + dir
+		} else {
+			cmd.Dir = path
+		}
+		err := cmd.Run()
+
+		if err != nil {
+			stream_print(app + " " + strings.Join(args, " "))
+			stream_print(fmt.Sprint(err) + ": " + stderr.String())
+			time.Sleep(1)
+		} else {
+			return out.String()
+		}
 	}
-	err := cmd.Run()
 
-	if err != nil {
-		stream_print(app + " " + strings.Join(args, " "))
-		stream_print(fmt.Sprint(err) + ": " + stderr.String())
-		os.Exit(1)
-	}
-
-	return out.String()
+	return "nope"
 }
 
 func clone(name string, url string) {
 	if _, err := os.Stat("repos/" + name); os.IsNotExist(err) {
-		stream_print(execute("/usr/bin/git", name, []string{"clone", url, "repos/" + name}))
+		res := execute("/usr/bin/git", name, []string{"clone", url, "repos/" + name})
+		if res == "nope" {
+			stream_print("Problem cloning " + url + ", skipping ...")
+			return
+		}
+		stream_print(res)
 	}
 }
 
 func pull(name string) {
 	res := strings.Trim(execute("/usr/bin/git", name, []string{"pull", "--rebase"}), "\n")
+	if res == "nope" {
+		stream_print("Problem pulling, skipping ...")
+		return
+	}
 	if res != "Already up to date." {
 		stream_print(res)
 	}
 }
 
 func getLastSHA1(name string) string {
-	return strings.TrimSuffix(execute("/usr/bin/git", name, []string{"rev-parse", "--verify", "HEAD"}), "\n")
+	res := execute("/usr/bin/git", name, []string{"rev-parse", "--verify", "HEAD"})
+	if res == "nope" {
+		stream_print("Problem with rev-parse on " + name + ", skipping ...")
+		return res
+	}
+	return strings.TrimSuffix(res, "\n")
 }
 
 func getLastSHA1User(name string, user string) string {
-	return strings.TrimSuffix(execute("/usr/bin/git", name, []string{"log", "-i", "--author", user, "-n", "1", "--pretty=format:\"%H\""}), "\n")[1:41]
+	res := execute("/usr/bin/git", name, []string{"log", "-i", "--author", user, "-n", "1", "--pretty=format:\"%H\""})
+	if res == "nope" {
+		stream_print("Problem with log on " + name + ", skipping ...")
+		return res
+	}
+	return strings.TrimSuffix(res, "\n")[1:41]
 }
 
 func getCommitTitle(name string, sha1Commit string) string {
-	review_title := strings.Split(execute("/usr/bin/git", name, []string{"show", "--name-only", "--pretty=format:\"%ad || %s %d || %an\"", "--date=short", sha1Commit}), "\n")[0]
+	res := execute("/usr/bin/git", name, []string{"show", "--name-only", "--pretty=format:\"%ad || %s %d || %an\"", "--date=short", sha1Commit})
+	if res == "nope" {
+		stream_print("Problem show on " + name + ", skipping ...")
+		return res
+	}
+	review_title := strings.Split(res, "\n")[0]
 	review_title = review_title[1 : len(review_title)-1]
 	return review_title
 }
@@ -89,7 +118,13 @@ func removeDuplicateStr(strSlice []string) []string {
 }
 
 func print_commits(index int, name string, url string, sha1 string, provider string) {
-	commits := strings.Split(execute("/usr/bin/git", name, []string{"rev-list", sha1 + "..HEAD"}), "\n")
+	res := execute("/usr/bin/git", name, []string{"rev-list", sha1 + "..HEAD"})
+	if res == "nope" {
+		stream_print("Problem with rev-list on " + url + ", skipping ...")
+		return
+	}
+
+	commits := strings.Split(res, "\n")
 	if len(commits) > 0 {
 		stream_print("")
 	}
@@ -115,7 +150,13 @@ func print_commits(index int, name string, url string, sha1 string, provider str
 }
 
 func print_commits_user(index int, user_index int, name string, url string, user string, sha1 string, provider string) {
-	commits := strings.Split(execute("/usr/bin/git", name, []string{"log", "-i", "--author", user, "-n", "100", "--pretty=format:\"%H\"", sha1 + "..HEAD"}), "\n")
+	res := execute("/usr/bin/git", name, []string{"log", "-i", "--author", user, "-n", "100", "--pretty=format:\"%H\"", sha1 + "..HEAD"})
+	if res == "nope" {
+		stream_print("Problem with log on " + url + ", skipping ...")
+		return
+	}
+
+	commits := strings.Split(res, "\n")
 	if len(commits) > 0 {
 		stream_print("")
 	}
@@ -157,6 +198,9 @@ func update_repos() {
 
 		if len(repos.Repos[i].Users) == 0 {
 			last_commit := getLastSHA1(name)
+			if last_commit == "nope" {
+				continue
+			}
 			if repos.Repos[i].LastSHA1 == "" {
 				repos.Repos[i].LastSHA1 = last_commit
 			}
@@ -168,8 +212,11 @@ func update_repos() {
 
 		for t := 0; t < len(repos.Repos[i].Users); t++ {
 			last_commit := getLastSHA1User(name, repos.Repos[i].Users[t].Username)
+			if last_commit == "nope" {
+				continue
+			}
 			if repos.Repos[i].Users[t].LastSHA1 == "" {
-				repos.Repos[i].Users[t].LastSHA1 = getLastSHA1User(name, repos.Repos[i].Users[t].Username)
+				repos.Repos[i].Users[t].LastSHA1 = last_commit
 			}
 			stream_print(fmt.Sprintf(" %2s) %-7s %-7s %-9s %-41s %s", strconv.Itoa(i), repos.Repos[i].Provider, repos.Repos[i].Branch, repos.Repos[i].Users[t].Username, repos.Repos[i].Users[t].LastSHA1, repos.Repos[i].Url))
 			if last_commit != repos.Repos[i].Users[t].LastSHA1 {
